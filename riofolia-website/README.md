4d2f9a75d53ba52df60560238cdc89faa43db34a
Julio Zynger <julio.zynger@soundcloud.com>
2020-11-01 23:40:42 +0100

    Add Rio Folia Website

---
+++ riofolia-website/README.md
@@ -0,0 +1,5 @@
+# Rio Folia Website
+
+Hosted on http://riofolia.droppages.com/
+
+![still](capture.png)
---
+++ riofolia-website/riofolia.droppages.com/Public/riofolia_files/j.js
@@ -0,0 +1,9 @@
+$(function() {
+	$('#slider').nivoSlider({
+		effect: 'fade',
+		controlNav: false,
+		directionNav: false,
+		pauseTime: 4500,
+		animSpeed: 500
+	});
+});
